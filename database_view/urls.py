8dffdb6830e78a780aed155df76cab4d519efd4e
Viktor Repin <hiddenstmail@gmail.com>
2021-06-01 23:36:09 +0700

    Fix bugs

--- database_view/urls.py
+++ database_view/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import index
-from .views import AvailableRooms
+from .views import AvailableRooms, MyServices, personal_area, search_hotels
 
 urlpatterns = [
     path('', index, name='index'),
     path('availableRooms/', AvailableRooms.as_view(), name='available_rooms'),
     path('searchRooms/', AvailableRooms.as_view(), name='search_rooms'),
-    path('searchHotels/', AvailableRooms.as_view(), name='search_hotels'),
+    path('searchHotels/', search_hotels, name='search_hotels'),
     path('services/', AvailableRooms.as_view(), name='services_list'),
-    path('myServices/', AvailableRooms.as_view(), name='my_services_list'),
-    path('personalArea/', AvailableRooms.as_view(), name='personal_area')
+    path('myServices/', MyServices.as_view(), name='my_services_list'),
+    path('personalArea/', personal_area, name='personal_area')
 
 ]
--- database_view/views.py
+++ database_view/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from database_view.models.room_model import RoomModel
+from database_view.models.service_model import ServiceModel
 
 def index(request):
     return render(request, 'menu.html')
@@ -12,4 +13,15 @@ class AvailableRooms(ListView):
     model = RoomModel
     template_name = 'available_rooms/available_roms.html'
     queryset = RoomModel.objects.all()
+
+class MyServices(ListView):
+    model = ServiceModel
+    template_name = 'my_services/my_services_list.html'
+    queryset = ServiceModel.objects.all()
+
+def personal_area(request):
+    return render(request, 'personal_area/personal_area_list.html')
+
+def search_hotels(request):
+    return render(request, 'search_hotels/search_hotels.html')
 
