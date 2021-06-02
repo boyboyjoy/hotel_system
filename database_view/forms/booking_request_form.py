dced7aa6b26bbb8d220f853a9676a058cedfceb8
Viktor Repin <hiddenstmail@gmail.com>
2021-06-02 22:21:45 +0700

    Add booking logic

--- database_view/forms/booking_request_form.py
+++ database_view/forms/booking_request_form.py
@@ -2,5 +2,7 @@ from django.forms import Form, CharField, DateTimeField, DateTimeInput, ModelCho
 from database_view.models import HotelModel, HotelClassModel, HotelsChainModel, CityModel
 
 class BookingRequestForm(Form):
-    check_in_date = DateTimeField(label='Дата заселения', widget=DateTimeInput)
-    check_out_date = DateTimeField(label='Дата выселения', widget=DateTimeInput)
+    check_in_date = DateTimeField(label='Дата заселения', widget=DateTimeInput(
+                attrs={'type': 'datetime-local'}))
+    check_out_date = DateTimeField(label='Дата выселения', widget=DateTimeInput(
+                attrs={'type': 'datetime-local'}))
